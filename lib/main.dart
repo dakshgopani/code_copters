9505e40d92a53b9f19a5699c6c3a2d7f880a71c8
Rishi Vejani <123580547+rishivejani15@users.noreply.github.com>
2024-10-04 23:13:16 +0530

    login saved

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:event_planner/custom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart'; // If you're using Provider
 import 'auth_screen.dart'; // Import your AuthScreen
 import 'language_state.dart'; // Import your LanguageState or other providers
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Check if the user is logged in before showing AuthScreen
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool? isLoggedIn = prefs.getBool('isLoggedIn');
+
   runApp(
     MultiProvider(
       providers: [
@@ -11,12 +19,15 @@ void main() {
             create: (_) => LanguageState()), // Example provider
         // Add other providers if necessary
       ],
-      child: MyApp(),
+      child: MyApp(isLoggedIn: isLoggedIn ?? false),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
+  final bool isLoggedIn;
+  MyApp({required this.isLoggedIn});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +36,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.orange,
       ),
       debugShowCheckedModeBanner: false,
-      home: AuthScreen(), // Start with the AuthScreen
+      home: isLoggedIn
+          ? CustomNavigationBar()
+          : AuthScreen(), // Show HomeScreen if logged in, otherwise AuthScreen
     );
   }
 }
